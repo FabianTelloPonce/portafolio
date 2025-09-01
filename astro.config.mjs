5e6266db92edd0fe8aac45337cacd53ee7cb27f9
FabianTelloPonce <a2193266076@alumnos.uat.edu.mx>
2025-09-01 09:41:27 -0600

    Configurar para dominio personalizado fabianciro.tech

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://FabianTelloPonce.github.io',
-  base: '/portafolio',
+  site: 'https://fabianciro.tech',
+  base: '/',
   output: 'static'
 });
